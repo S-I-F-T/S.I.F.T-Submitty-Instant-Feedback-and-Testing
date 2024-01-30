1a4a772544eeea96fd0a7f35bd5162dc66011eac
Ayaan31 <ahmada5@rpi.edu>
2024-01-30 17:42:21 -0500

    Testing compiling java code

--- subprocess-test.py
+++ subprocess-test.py
@@ -1,6 +1,5 @@
 import subprocess
 
+path = "javac /Users/ahmad/Desktop/OneDrive - Rensselaer Polytechnic Institute/RPI(S24)/psoft/homeworks/hw0/src/test/java/hw0/BoxTest.java"
 
-subprocess.run('javac test.java', shell=True)
-
-subprocess.run('java test', shell=True)
+subprocess.Popen(path, shell=True)
