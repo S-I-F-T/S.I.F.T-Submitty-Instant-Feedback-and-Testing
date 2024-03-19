472858e75067227bc493b771c4e5756938999738
Abzak <abzak1230@gmail.com>
2024-03-19 16:48:26 -0400

    fixing errors in displaying about page

--- sift_ui.py
+++ sift_ui.py
@@ -63,7 +63,7 @@ button_frame_2.pack(pady = 12, padx = 10)
 
 about_button = ctk.CTkButton(master = button_frame_2, text = "About",
                              font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                             fg_color="#575757", width = 255, height = 55)
+                             fg_color="#575757", width = 255, height = 55, command = lambda: switch_frame('about_frame'))
 about_button.pack(side=ctk.LEFT, padx = 15)
 
 settings_button = ctk.CTkButton(master = button_frame_2, text = "Settings",
@@ -166,6 +166,65 @@ settings_back_button = ctk.CTkButton(master = settings_frame, text = "Back",
                                           fg_color="#575757", width = 100, height = 40, 
                                           command = lambda: switch_frame('welcome_frame'))
 settings_back_button.pack(side=ctk.BOTTOM, pady = 10)
+
+# About page
+about_frame = ctk.CTkFrame(master = root, fg_color="#343434", bg_color="#343434")
+
+about_title = ctk.CTkLabel(master = about_frame, text = 'About SIFT',
+                           font = ctk.CTkFont(family='Calibri', size=30, weight = "bold"))
+about_title.pack(padx=10, pady=10)
+
+description1 = """SIFT (Submitty Instant Feedback and Testing) is a standalone auto-grader
+for coding homework assignments to help students test their code
+effectively without relying on Submitty servers"""
+about_paragraph1 = ctk.CTkLabel(master = about_frame, text = description1,
+                                font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
+about_paragraph1.pack(padx=10, pady=5)
+
+description2 = "Created by Abedalah Safi, Abrar Zaki, Ayaan Ahmad, and Josh Javillo"
+about_paragraph2 = ctk.CTkLabel(master = about_frame, text = description2,
+                                font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
+about_paragraph2.pack(padx=10, pady=5)
+
+description3 = """MIT License
+Copyright (c) 2024 S.I.F.T v0.0.1"""
+about_paragraph3 = ctk.CTkLabel(master = about_frame, text = description3,
+                                font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
+about_paragraph3.pack(padx=10, pady=5)
+
+about_back_button = ctk.CTkButton(master = about_frame, text = "Back",
+                                        font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
+                                        fg_color="#575757", width = 100, height = 40, 
+                                        command = lambda: switch_frame('welcome_frame'))
+about_back_button.pack(side=ctk.BOTTOM, pady = 10)
+
+# How to use page
+how_to_frame = ctk.CTkFrame(master = root, fg_color="#343434", bg_color="#343434")
+
+how_to_title = ctk.CTkLabel(master = how_to_frame, text = 'How to Use SIFT',
+                                font = ctk.CTkFont(family='Calibri', size=30, weight = "bold"))
+how_to_title.pack(padx=10, pady=10)
+
+ht_description1 = """SIFT allows users to auto-grade their homework
+assignments without fear of wasting submissions
+and waiting for Submitty servers to process code"""
+how_to_paragraph1 = ctk.CTkLabel(master = how_to_frame, text = description1, font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
+
+how_to_paragraph1.pack(padx=10, pady=5)
+
+description2 = """To use SIFT, navigate to the Autograder page and
+input your own code, as well as the expected output
+files for the assignment
+    Once you press the “Compare Output” button,
+SIFT will run your code and compare it against the
+expected output files provided
+The comparison will highlight discrepancies between
+the output of the provided code and the expected 
+output for easy bug finding and fixing"""
+paragraph2 = ctk.CTkLabel(master = how_to_frame, text=description2, font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
+
+# paragraph2.pack(padx=10, pady=10)
+
 
 # Pack the welcome frame
 welcome_frame.pack(fill = 'both', expand = True)
