4c965e7f8bce0cfb089586c0d4e56051e804f253
javilj <javilj@rpi.edu>
2024-03-22 17:47:32 -0400

    make changes to allow custom themes and make custom themes

--- sift_ui.py
+++ sift_ui.py
@@ -21,7 +21,7 @@ def select_expected_file(expected_files):
         expected_files.append(filename)
 
 ctk.set_appearance_mode('dark')
-ctk.set_default_color_theme('dark-blue')
+ctk.set_default_color_theme('./frontend/themes/green.json')
 
 root = ctk.CTk()
 root.geometry('640x480')
@@ -54,52 +54,52 @@ expectedfiles = []
 # Welcome page
 current_frame = "welcome_frame"
 
-welcome_frame = ctk.CTkFrame(master = root, fg_color="#343434", bg_color="#343434")
+welcome_frame = ctk.CTkFrame(master = root)
 
 welcome_title = ctk.CTkLabel(master = welcome_frame, text = 'Welcome to SIFT', 
                      font = ctk.CTkFont(family='Calibri', size = 30, weight = "bold"))
 welcome_title.pack(pady = 12, padx = 10)
 
-button_frame_1 = ctk.CTkFrame(master= welcome_frame, fg_color="#343434", bg_color="#343434")
+button_frame_1 = ctk.CTkFrame(master= welcome_frame)
 button_frame_1.pack(pady = 12, padx = 10)
 
 autograder_button = ctk.CTkButton(master = button_frame_1, text = "Autograder",
                                   font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                                  fg_color = "#575757", width = 255, height = 55, 
+                                   width = 255, height = 55, 
                                   command = lambda: switch_frame('file_select_frame'))
 autograder_button.pack(side=ctk.LEFT, padx = 15)
 
 how_to_button = ctk.CTkButton(master = button_frame_1, text = "How to Use", 
                               font = ctk.CTkFont(family = 'Calibri', size = 15, weight = "bold"),
-                              fg_color="#575757", width = 255, height = 55, command = lambda: switch_frame('how_to_frame'))
+                               width = 255, height = 55, command = lambda: switch_frame('how_to_frame'))
 
 how_to_button.pack(side=ctk.RIGHT, padx = 15)
 
-button_frame_2 = ctk.CTkFrame(master= welcome_frame, fg_color="#343434", bg_color="#343434")
+button_frame_2 = ctk.CTkFrame(master= welcome_frame)
 button_frame_2.pack(pady = 12, padx = 10)
 
 about_button = ctk.CTkButton(master = button_frame_2, text = "About",
                              font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                             fg_color="#575757", width = 255, height = 55, command = lambda: switch_frame('about_frame'))
+                              width = 255, height = 55, command = lambda: switch_frame('about_frame'))
 about_button.pack(side=ctk.LEFT, padx = 15)
 
 settings_button = ctk.CTkButton(master = button_frame_2, text = "Settings",
                                 font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                                fg_color="#575757", width = 255, height = 55, command = lambda: switch_frame('settings_frame'))
+                                 width = 255, height = 55, command = lambda: switch_frame('settings_frame'))
 settings_button.pack(side=ctk.RIGHT, padx = 15)
 
-quit_frame = ctk.CTkFrame(master = welcome_frame, fg_color="#343434", bg_color="#343434")
+quit_frame = ctk.CTkFrame(master = welcome_frame)
 quit_frame.pack(pady = 12, padx = 10)
 
 quit_button = ctk.CTkButton(master = quit_frame, text = "Quit",
                                 font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                                fg_color="#575757", width = 255, height = 55, command = root.quit)
+                                 width = 255, height = 55, command = root.quit)
 quit_button.pack(side=ctk.BOTTOM, pady = 10)
 
 # File select page
-file_select_frame = ctk.CTkFrame(master = root, fg_color="#343434", bg_color="#343434")
+file_select_frame = ctk.CTkFrame(master = root)
 
-file_select_title_frame = ctk.CTkFrame(master = file_select_frame, fg_color="#343434", bg_color="#343434")
+file_select_title_frame = ctk.CTkFrame(master = file_select_frame)
 file_select_title_frame.pack(pady = 12, padx = 10)
 
 your_files_title = ctk.CTkLabel(master = file_select_title_frame, text = 'Your file(s)', font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
@@ -108,41 +108,41 @@ your_files_title.pack(side=ctk.LEFT, padx = 50)
 expected_files_title = ctk.CTkLabel(master = file_select_title_frame, text = 'Expected output file(s)', font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
 expected_files_title.pack(side=ctk.RIGHT, padx = 150)
 
-file_select_choose_frame = ctk.CTkFrame(master = file_select_frame, fg_color="#343434", bg_color="#343434")
+file_select_choose_frame = ctk.CTkFrame(master = file_select_frame)
 file_select_choose_frame.pack(pady = 12, padx = 10)
 
 your_files_button = ctk.CTkButton(master = file_select_choose_frame, text = "Select your file(s)", 
                                   font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                                  fg_color="#575757", width = 255, height = 55, command = select_user_file)
+                                   width = 255, height = 55, command = select_user_file)
 your_files_button.pack(side=ctk.LEFT, padx = 15)
 
 expected_files_button = ctk.CTkButton(master = file_select_choose_frame, text = "Select expected output file(s)",
                                         font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                                        fg_color="#575757", width = 255, height = 55, command = select_expected_file)
+                                         width = 255, height = 55, command = select_expected_file)
 expected_files_button.pack(side=ctk.RIGHT, padx = 15)
 
 file_select_back_button = ctk.CTkButton(master = file_select_frame, text = "Back", 
                                        font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"), 
-                                       fg_color="#575757", width = 100, height = 40, 
+                                        width = 100, height = 40, 
                                        command = lambda: switch_frame('welcome_frame'))
 file_select_back_button.pack(side=ctk.BOTTOM, pady = 10)
 
 file_select_compare_button = ctk.CTkButton(master = file_select_frame, text = "Compare",
                                             font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                                            fg_color="#575757", width = 100, height = 40, command= lambda: switch_frame('comparison_frame'))
+                                             width = 100, height = 40, command= lambda: switch_frame('comparison_frame'))
 file_select_compare_button.pack(side=ctk.BOTTOM, pady = 10)
 
 # Comparison page
-comparison_frame = ctk.CTkFrame(master = root, fg_color="#343434", bg_color="#343434")
+comparison_frame = ctk.CTkFrame(master = root)
 
-comparison_title_frame = ctk.CTkFrame(master = comparison_frame, fg_color="#343434", bg_color="#343434")
+comparison_title_frame = ctk.CTkFrame(master = comparison_frame)
 comparison_title_frame.pack(pady = 12, padx = 10)
 
 comparison_title = ctk.CTkLabel(master = comparison_title_frame, text = 'Comparison',
                                 font = ctk.CTkFont(family='Calibri', size=30, weight = "bold"))
 comparison_title.pack(pady = 12, padx = 10)
 
-comparison_title_frame = ctk.CTkFrame(master = comparison_frame, fg_color="#343434", bg_color="#343434")
+comparison_title_frame = ctk.CTkFrame(master = comparison_frame)
 comparison_title_frame.pack(pady = 12, padx = 10)
 
 your_output_title = ctk.CTkLabel(master = comparison_title_frame, text = 'Your output', font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
@@ -153,18 +153,18 @@ expected_output_title.pack(side=ctk.RIGHT, padx = 150)
 
 comparison_back_button = ctk.CTkButton(master = comparison_frame, text = "Back", 
                                        font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"), 
-                                       fg_color="#575757", width = 100, height = 40, 
+                                        width = 100, height = 40, 
                                        command = lambda: switch_frame('file_select_frame'))
 comparison_back_button.pack(side=ctk.BOTTOM, pady = 10)
 
 # Settings page
-settings_frame = ctk.CTkFrame(master = root, fg_color="#343434", bg_color="#343434")
+settings_frame = ctk.CTkFrame(master = root)
 
 settings_title = ctk.CTkLabel(master = settings_frame, text = 'Settings',
                                 font = ctk.CTkFont(family='Calibri', size=30, weight = "bold"))
 settings_title.pack(pady = 12, padx = 10)
 
-settings_language_frame = ctk.CTkFrame(master = settings_frame, fg_color="#343434", bg_color="#343434")
+settings_language_frame = ctk.CTkFrame(master = settings_frame)
 settings_language_frame.pack(pady = 12, padx = 10)
 
 settings_language_label = ctk.CTkLabel(master = settings_language_frame, text = 'Language:',
@@ -175,7 +175,7 @@ settings_language_dropdown = ctk.CTkOptionMenu(master = settings_language_frame,
                                                 font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
 settings_language_dropdown.pack(side=ctk.RIGHT, padx = 150)
 
-settings_theme_frame = ctk.CTkFrame(master = settings_frame, fg_color="#343434", bg_color="#343434")
+settings_theme_frame = ctk.CTkFrame(master = settings_frame)
 settings_theme_frame.pack(pady = 12, padx = 10)
 
 settings_theme_label = ctk.CTkLabel(master = settings_theme_frame, text = 'Theme:',
@@ -188,12 +188,12 @@ settings_theme_dropdown.pack(side=ctk.RIGHT, padx = 150)
 
 settings_back_button = ctk.CTkButton(master = settings_frame, text = "Back",
                                           font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                                          fg_color="#575757", width = 100, height = 40, 
+                                           width = 100, height = 40, 
                                           command = lambda: switch_frame('welcome_frame'))
 settings_back_button.pack(side=ctk.BOTTOM, pady = 10)
 
 # About page
-about_frame = ctk.CTkFrame(master = root, fg_color="#343434", bg_color="#343434")
+about_frame = ctk.CTkFrame(master = root)
 
 about_title = ctk.CTkLabel(master = about_frame, text = 'About SIFT',
                            font = ctk.CTkFont(family='Calibri', size=30, weight = "bold"))
@@ -211,6 +211,7 @@ about_paragraph2 = ctk.CTkLabel(master = about_frame, text = description2,
                                 font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"))
 about_paragraph2.pack(padx=10, pady=5)
 
+
 description3 = """MIT License
 Copyright (c) 2024 S.I.F.T v0.0.1"""
 about_paragraph3 = ctk.CTkLabel(master = about_frame, text = description3,
@@ -219,12 +220,12 @@ about_paragraph3.pack(padx=10, pady=5)
 
 about_back_button = ctk.CTkButton(master = about_frame, text = "Back",
                                         font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                                        fg_color="#575757", width = 100, height = 40, 
+                                         width = 100, height = 40, 
                                         command = lambda: switch_frame('welcome_frame'))
 about_back_button.pack(side=ctk.BOTTOM, pady = 10)
 
 # How to use page
-how_to_frame = ctk.CTkFrame(master = root, fg_color="#343434", bg_color="#343434")
+how_to_frame = ctk.CTkFrame(master = root)
 
 how_to_title = ctk.CTkLabel(master = how_to_frame, text = 'How to Use SIFT',
                                 font = ctk.CTkFont(family='Calibri', size=30, weight = "bold"))
@@ -247,7 +248,7 @@ paragraph2.pack(padx=10, pady=10)
 
 how_to_back_button = ctk.CTkButton(master = how_to_frame, text = "Back",
                                         font = ctk.CTkFont(family='Calibri', size=15, weight = "bold"),
-                                        fg_color="#575757", width = 100, height = 40, 
+                                         width = 100, height = 40, 
                                         command = lambda: switch_frame('welcome_frame'))
 how_to_back_button.pack(side=ctk.BOTTOM, pady = 10)
 
