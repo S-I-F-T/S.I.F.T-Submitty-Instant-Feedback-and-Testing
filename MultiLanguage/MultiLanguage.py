e08ca4c38aa972c2b342671a5ff2055cbea1a64e
AbeSafi <137196005+AbeSafi@users.noreply.github.com>
2024-03-22 17:11:15 -0400

    Added Settings CSV

--- MultiLanguage/MultiLanguage.py
+++ MultiLanguage/MultiLanguage.py
@@ -1,18 +1,18 @@
 import csv
-import json
+# import json
 
 print("Hello")
 
-# with open('sifttings.csv', 'r') as csv_file:
-#   settings = csv.reader(csv_file)
+with open('sifttings.csv', 'r') as csv_file:
+  settings = csv.reader(csv_file)
   
-#   for line in settings:
-#     print(line)
+  for line in settings:
+    print(line)
 
-with open('sifttings.json', 'r') as j:
-  settings = json.load(j)
+# with open('sifttings.json', 'r') as j:
+#   settings = json.load(j)
 
-  print(settings)
+#   print(settings)
 
 # class MultiLanguage():
 #   def __init__(self):
