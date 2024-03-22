c9ae1f8f74999fcf447107adb63e98361e28cfb2
AbeSafi <137196005+AbeSafi@users.noreply.github.com>
2024-03-22 17:08:46 -0400

    Running Debug Tests

--- MultiLanguage/MultiLanguage.py
+++ MultiLanguage/MultiLanguage.py
@@ -1,12 +1,27 @@
+import csv
 import json
 
-class MultiLanguage():
-  def __init__(self):
-    with open("sifttings", "r") as j:
-      data = json.load(j)
-      
-      print(data)
+print("Hello")
 
-if __name__ == "__main__":
-  multi_language = MultiLanguage()
+# with open('sifttings.csv', 'r') as csv_file:
+#   settings = csv.reader(csv_file)
+  
+#   for line in settings:
+#     print(line)
+
+with open('sifttings.json', 'r') as j:
+  settings = json.load(j)
+
+  print(settings)
+
+# class MultiLanguage():
+#   def __init__(self):
+#     with open('sifttings.csv', 'r') as csv_file:
+#       settings = csv.reader(csv_file)
+      
+#       for line in settings:
+#         print(line)
+
+# if __name__ == "__main__":
+#   multi_language = MultiLanguage()
 
